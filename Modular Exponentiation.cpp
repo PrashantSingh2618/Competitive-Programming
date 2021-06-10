496b9ad191242a39ac741498b01e5cdc203df9c9
PrashantSingh2618 <38156926+PrashantSingh2618@users.noreply.github.com>
2021-06-11 02:35:08 +0530

    Create Modular Exponentiation.cpp

---
+++ Modular Exponentiation.cpp
@@ -0,0 +1,15 @@
+// call using power (x,y,p)    to cal  (x^y)%p
+ll power(ll x, ll y,ll p) 
+{ 
+    ll res = 1;
+    x = x % p;  
+  
+    while (y > 0) 
+    { 
+        if (y & 1) 
+            res = (res*x) % p; 
+        y = y>>1; 
+        x = (x*x) % p;   
+    } 
+    return res; 
+} 
