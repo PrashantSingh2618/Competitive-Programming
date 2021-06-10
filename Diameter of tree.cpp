42b44d6f24346ee2b70e8286d16533c09ad2aec2
PrashantSingh2618 <38156926+PrashantSingh2618@users.noreply.github.com>
2021-06-11 02:16:25 +0530

    Create Diameter of tree.cpp

---
+++ Diameter of tree.cpp
@@ -0,0 +1,34 @@
+
+void dfs(vt<vt<ll>>&adj , vt<ll>&visited ,ll u,vt<ll>&level)
+{
+    visited[u] = 1;
+    EACH(it,adj[u])
+    {
+        if(!visited[it])
+        {
+            level[it]=level[u]+1;
+            dfs(adj,visited,it,level);
+        }
+    }
+}
+ll diameter(vt<vt<ll>>&adj)
+{
+   vt<ll>level(n,0),visited(n,0);
+  dfs(adj,visited,0,level);
+ 
+  ll mx = *max_element(all(level));
+  rep(i,0,n)
+  {
+    if(level[i]==mx)
+    {
+      mx = i;
+      break;
+    }
+  }
+  vt<ll>level1(n),visited1(n);
+       
+  dfs(adj,visited1,mx,level1);
+  ll dia = *max_element(all(level1));
+  return dia ;
+}
+ 
