c4faee9ad4032326b8e5c770a6d40258f577571d
Prashant Singh <38156926+PrashantSingh2618@users.noreply.github.com>
2023-03-11 00:02:11 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,35 +1,47 @@
+class DSU {
+    int N;
+public:
+    vector<int> parent, size;
 
-ll root (vector<ll>&parent ,ll i)
-{
-    while(parenty[i] != i)
-    {
-        parent[i] = parent[parent[i]] ; 
-        i = parent[i]; 
-    }
-    return i;
-}
-void union(vector<ll>&parent,vector<ll>&size,ll A, ll B)
-{
-    ll root_A = root(parent,A);
-    ll root_B = root(parent,B);
-    if(size[root_A] < size[root_B])
-    {
-        parent[ root_A ] = parent[root_B];
-        size[root_B] += size[root_A];
-    }
-    else
-    {
-        parent[ root_B ] = parent[root_A];
-        size[root_A] += size[root_B];
+    DSU(int n){
+        N = n;
+        par.resize(N);
+        size.resize(N);
     }
 
-}
-// Call DSU_init to initilise initial values
-void DSU_init(vector<ll>&parent,vector<ll>&size,ll n)
-{
-    for(int i = 0 ; i < n;i++)
+    void init()
     {
-        parent[i] = i ;
-        size[i] = 1;
+        for(int i = 0 ; i < N;i++)
+        {
+            parent[i] = i ;
+            size[i] = 1;
+        }
     }
-}
+
+    int root(int i)
+    {
+        while(parent[i] != i)
+        {
+            parent[i] = parent[parent[i]] ; 
+            i = parent[i]; 
+        }
+        return i;
+    }
+
+    void union(int A, int B)
+    {
+        int root_A = root(parent,A);
+        int root_B = root(parent,B);
+        if(size[root_A] < size[root_B])
+        {
+            parent[ root_A ] = parent[root_B];
+            size[root_B] += size[root_A];
+        }
+        else
+        {
+            parent[ root_B ] = parent[root_A];
+            size[root_A] += size[root_B];
+        }
+
+    }
+};
