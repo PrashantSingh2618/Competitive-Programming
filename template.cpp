38af09e65278346d5e50c2ff61f9ffbcedef7683
Prashant Singh <38156926+PrashantSingh2618@users.noreply.github.com>
2025-07-23 19:49:05 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,7 +8,7 @@ using namespace std ;
 #define rev(v) reverse(v)
 #define sz(x) (int)(x).size()
 #define ll long long
-#define F first 
+#define F first
 #define S second
 #define pll pair<ll,ll>
 #define ld long double
@@ -17,13 +17,13 @@ using namespace std ;
 #define onf() ofstream cout("test.txt")
 #define mod 1000000007
 #define pb push_back
-#define nl cout<<"\n";
+#define nl "\n";
 #define prt1(a) cout<<a<<" "
 #define prt2(a,b) cout<<a<<" "<<b<<" "
 #define prt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<" "
 #define in(a) cin>>a
 #define inn(a,b) cin>>a>>b
-#define innn(a,b,c) cin>>a>>b>>c 
+#define innn(a,b,c) cin>>a>>b>>c
 #define rep(i,st,end) for(ll i=st;i<end;i++)
 #define repv(i,st,end) for(ll i=st;i>=end;i--)
 #define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
@@ -42,12 +42,12 @@ template<class T> void read(T& x) {
 void read(double& d) {
     string t;
     read(t);
-    d=stod(t);
+    d = stod(t);
 }
 void read(long double& d) {
     string t;
     read(t);
-    d=stold(t);
+    d = stold(t);
 }
 template<class H, class... T> void read(H& h, T&... t) {
     read(h);
@@ -55,60 +55,55 @@ template<class H, class... T> void read(H& h, T&... t) {
 }
 template<class A> void read(vt<A>& x) {
     EACH(a, x)
-        read(a);
+    read(a);
 }
 template<class A, size_t S> void read(array<A, S>& x) {
     EACH(a, x)
-        read(a);
+    read(a);
 }
 #define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
 
 void err(istream_iterator<string> it) {}
 template<typename T, typename... Args>
 void err(istream_iterator<string> it, T a, Args... args) {
-    cerr << *it << " = " << a <<"\n";
+    cerr << *it << " = " << a << "\n";
     err(++it, args...);
 }
-inline int add(int x, int y){ x += y; if(x >= mod) x -= mod; return x;}
-inline int sub(int x, int y){ x -= y; if(x < 0) x += mod; return x;}
-inline int mul(int x, int y){ return (x * 1ll * y) % mod;}
-inline int powr(int a, ll b){
+inline int add(int x, int y) { x += y; if (x >= mod) x -= mod; return x;}
+inline int sub(int x, int y) { x -= y; if (x < 0) x += mod; return x;}
+inline int mul(int x, int y) { return (x * 1ll * y) % mod;}
+inline int powr(int a, ll b) {
     int x = 1 % mod;
-    while(b){
-        if(b & 1) x = mul(x, a);
+    while (b) {
+        if (b & 1) x = mul(x, a);
         a = mul(a, a);
         b >>= 1;
     }
     return x;
 }
-inline int inv(int a){ return powr(a, mod - 2);}
+inline int inv(int a) { return powr(a, mod - 2);}
 
 void solve()
 {
     
+
 }
 
- int main()
- {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt", "w", stdout); 
-    #endif
-     
-    clock_t start, end; 
-    start = clock(); 
+int main()
+{
+    clock_t start, end;
+    start = clock();
     fio;
-    ll k = 1;
     ll tc = 1;
-    cin>>tc;
-    while(tc--){
-       // cout<<"Case #"<<k<<": ";k++;
+    cin >> tc;
+    while (tc--) {
+        // cout<<"Case #"<<k<<": ";k++;
         solve();
         nl;
-        
+
     }
     end = clock();
-     double time_taken = double(end - start) / double(CLOCKS_PER_SEC); 
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
     // debug(time_taken);
-    
- }
+
+}
